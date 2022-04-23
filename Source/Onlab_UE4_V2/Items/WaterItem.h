6f3c984adab0d4f7b3aaaa28aeadd5ec5997faf8
kabucz <karon20123@gmail.com>
2022-04-23 12:44:14 +0200

    water

---
+++ Source/Onlab_UE4_V2/Items/WaterItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "WaterItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONLAB_UE4_V2_API UWaterItem : public UItem
+{
+	GENERATED_BODY()
+	
+};
