0e057257e53104983a2c89a1c39a8a28a87f4814
kabucz <karon20123@gmail.com>
2022-04-23 12:04:14 +0200

    hadszoljonxd

---
+++ Source/Onlab_UE4_V2/Items/FoodItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "FoodItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONLAB_UE4_V2_API UFoodItem : public UItem
+{
+	GENERATED_BODY()
+	
+};
